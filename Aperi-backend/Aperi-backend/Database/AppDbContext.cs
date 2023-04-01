8654a9a18a6f9cd8fa6b39e4761c6822f3fdf02a
Haris Kordić <kordicharis18@gmail.com>
2023-04-02 01:01:13 +0200

    add: database class and initialization in program.cs

---
+++ Aperi-backend/Aperi-backend/Database/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Aperi_backend.Database;
+
+public class AppDbContext : DbContext
+{
+    #region Constructor
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+    #endregion
+
+    #region Model creating
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+    #endregion
+}
--- Aperi-backend/Aperi-backend/Program.cs
+++ Aperi-backend/Aperi-backend/Program.cs
@@ -1,19 +1,42 @@
+using Aperi_backend.Database;
+using Microsoft.EntityFrameworkCore;
+
+#region Database connection
+
+//Create a configuration with a path to appsettings.json file which holds all of the settings:
+
+
+var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json", false)
+                    .Build();
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Create a database context using SQL server with a connection string of "database"
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    _ = options.UseSqlServer(configuration.GetConnectionString("database"));
+    _ = options.UseLazyLoadingProxies();
+});
+#endregion
+
+#region Swagger initialization
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+#endregion
+
+#region App initialization
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    _ = app.UseSwagger();
+    _ = app.UseSwaggerUI();
 }
 
 app.UseAuthorization();
@@ -21,3 +44,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+#endregion
