d75000eb04f7a5aac38285b1530ec89953d5d9f4
Haris Kordić <kordicharis18@gmail.com>
2023-04-02 15:26:31 +0200

    fix: nfc entity

--- Aperi-backend/Aperi-backend/Database/Entities/NfcCardOrTag.cs
+++ Aperi-backend/Aperi-backend/Database/Entities/NfcCardOrTag.cs
@@ -6,8 +6,8 @@ namespace Aperi_backend.Database.Entities
     [Table("nfc_card_or_tag")]
     public class NfcCardOrTag
     {
-        [Column("Code"),StringLength(14)]
-        public string CodeId { get; set; }
+        [Column("id"),StringLength(14)]
+        public string Id { get; set; }
         [Column("created_at")]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
