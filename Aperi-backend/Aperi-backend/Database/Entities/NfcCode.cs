260cca4dc9bba68a38303bcdd4c9f2ea421ba8bb
Haris Kordić <kordicharis18@gmail.com>
2023-04-02 14:50:05 +0200

    add: nfc code entity

---
+++ Aperi-backend/Aperi-backend/Database/Entities/NfcCode.cs
@@ -0,0 +1,8 @@
+﻿namespace Aperi_backend.Database.Entities
+{
+    public class NfcCode
+    {
+        public string CodeId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
--- Aperi-backend/Aperi-backend/Database/Entities/User.cs
+++ Aperi-backend/Aperi-backend/Database/Entities/User.cs
@@ -2,7 +2,7 @@
 {
     public class User
     {
-        public int UserId { get; set; }
+        public int Id { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
