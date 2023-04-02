66a7d2281cf1c1b5d81e2d6c9c820d2e909d71a6
Haris Kordić <kordicharis18@gmail.com>
2023-04-02 15:09:56 +0200

    add: user seed

---
+++ Aperi-backend/Aperi-backend/Database/Seeds/UserSeed.cs
@@ -0,0 +1,26 @@
+﻿using Aperi_backend.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Aperi_backend.Database.Seeds
+{
+    public class UserSeed : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            _ = builder.HasData(
+                new User()
+                {
+                    Id = 1,
+                    FirstName = "Sara",
+                    LastName = "Nuredinovski",
+                },
+                new User()
+                {
+                    Id = 2,
+                    FirstName = "Haris",
+                    LastName = "Kordić",
+                });
+        }
+    }
+}
