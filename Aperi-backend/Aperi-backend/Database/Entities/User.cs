884bf1c4472b9acbcdef26a0ce40ecc77740f07a
Haris Kordić <kordicharis18@gmail.com>
2023-04-02 14:47:49 +0200

    add: user entity

---
+++ Aperi-backend/Aperi-backend/Database/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Aperi_backend.Database.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
