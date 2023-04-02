423e04a6e9943f1963d4ef9cd92742406feadfa5
Haris Kordić <kordicharis18@gmail.com>
2023-04-02 15:02:58 +0200

    fix: pk length in nfc code entity

--- Aperi-backend/Aperi-backend/Database/Entities/NfcCode.cs
+++ Aperi-backend/Aperi-backend/Database/Entities/NfcCode.cs
@@ -1,11 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Aperi_backend.Database.Entities
 {
     [Table("nfc_codes")]
     public class NfcCode
     {
-        [Column("id")]
+        [Column("id"),StringLength(14)]
         public string Id { get; set; }
         [Column("created_at")]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
