bb01cad41d125a0f1c4ecd80dcda15f53d4c704a
Haris Kordić <kordicharis18@gmail.com>
2023-04-02 17:03:58 +0200

    add: nfc seeds

---
+++ Aperi-backend/Aperi-backend/Database/Seeds/NfcSeed.cs
@@ -0,0 +1,22 @@
+﻿using Aperi_backend.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Aperi_backend.Database.Seeds
+{
+    public class NfcSeed : IEntityTypeConfiguration<NfcCardOrTag>
+    {
+        public void Configure(EntityTypeBuilder<NfcCardOrTag> builder)
+        {
+            _ = builder.HasData(
+                new NfcCardOrTag()
+                {
+                    Id = "95381EE7" //Card
+                },
+                new NfcCardOrTag()
+                {
+                    Id = "2DC951D3" //Tag
+                });
+        }
+    }
+}
