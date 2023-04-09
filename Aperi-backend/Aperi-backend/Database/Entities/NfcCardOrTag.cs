6a6887d3aabd265d37090adda5a3b3159b29100d
Haris Kordić <kordicharis18@gmail.com>
2023-04-09 14:16:34 +0200

    add: boolean flag for scanning

--- Aperi-backend/Aperi-backend/Database/Entities/NfcCardOrTag.cs
+++ Aperi-backend/Aperi-backend/Database/Entities/NfcCardOrTag.cs
@@ -6,8 +6,12 @@ namespace Aperi_backend.Database.Entities
     [Table("nfc_card_or_tag")]
     public class NfcCardOrTag
     {
-        [Column("id"),StringLength(8)]
-        public string Id { get; set; }
+        [Column("id"), StringLength(8)]
+        public string? Id { get; set; }
+
+        [Column("is_scanned")]
+        public bool IsScanned { get; set; } = false;
+
         [Column("created_at")]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
