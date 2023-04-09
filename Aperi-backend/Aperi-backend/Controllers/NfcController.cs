1c8f764be7525d3130af5f34fc10534bf03a5b27
Haris Kordić <kordicharis18@gmail.com>
2023-04-09 14:43:44 +0200

    add: first endpoint for scanning cards or tags

---
+++ Aperi-backend/Aperi-backend/Controllers/NfcController.cs
@@ -0,0 +1,33 @@
+﻿using Aperi_backend.Database;
+using Aperi_backend.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aperi_backend.Controllers
+{
+    [ApiController]
+    public class NfcController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly AppDbContext _appDbContext;
+        public NfcController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        [Route("api/scan/{nfcId}")]
+        public ActionResult<dtoScanned> Scan(string nfcId)
+        {
+            var scannedCard = _appDbContext.NfcCodes.Where(code => code.Id == nfcId).FirstOrDefault();
+            scannedCard!.IsScanned = true;
+
+            return scannedCard!.IsScanned ?
+                (ActionResult<dtoScanned>)
+                Ok(new dtoScanned() { IsScanned = true }) :
+                (ActionResult<dtoScanned>)
+                BadRequest(new dtoScanned() { IsScanned = false });
+
+        }
+
+    }
+}
---
+++ Aperi-backend/Aperi-backend/DTOs/dtoScanned.cs
@@ -0,0 +1,7 @@
+﻿namespace Aperi_backend.DTOs
+{
+    public class dtoScanned
+    {
+        public bool IsScanned { get; set; }
+    }
+}
