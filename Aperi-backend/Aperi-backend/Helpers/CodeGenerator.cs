e985a00efa80025c216872de5281dd5cf895d393
Sara Nuredinovski <saranur.cs@gmail.com>
2023-05-19 15:34:30 +0200

    add: email and code generation helper message

---
+++ Aperi-backend/Aperi-backend/Helpers/CodeGenerator.cs
@@ -0,0 +1,10 @@
+﻿namespace Aperi_backend.Helpers
+{
+    public static class CodeGenerator
+    {
+        public static string GetCode() {
+            var rand = new Random();
+            return rand.Next(0000, 9999).ToString();
+        }
+    }
+}
---
+++ Aperi-backend/Aperi-backend/Helpers/EmailHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Net.NetworkInformation;
+
+namespace Aperi_backend.Helpers
+{
+    public static class EmailHelper
+    {
+        public static void SendEmail(MailMessage message)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json");
+            var config = builder.Build();
+            var smtpClient = new SmtpClient(config["Smtp:Host"])
+            {
+                Port = int.Parse(config["Smtp:Port"]),
+                Credentials = new NetworkCredential(config["Smtp:Username"], config["Smtp:Password"]),
+                EnableSsl = true,
+            };
+            smtpClient.Send(message);
+        }
+    }
+}
